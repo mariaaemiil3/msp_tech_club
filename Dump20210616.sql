f17f99697b57d16a0c9e511e9de251dd6eb00871
Ahmed Abdelfattah <ama.elfattah@gmail.com>
2021-08-14 01:30:36 +0200

    Add workshops table

--- Dump20210616.sql
+++ Dump20210616.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.15.9
--- https://www.phpmyadmin.net
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 29, 2021 at 03:17 PM
--- Server version: 5.6.37
--- PHP Version: 5.6.31
+-- Generation Time: Aug 14, 2021 at 01:25 AM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `Dump20210616`
+-- Database: `msp_tech_club`
 --
 
 -- --------------------------------------------------------
@@ -26,8 +27,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `committees`
 --
 
-DROP TABLE IF EXISTS `committees`;
-CREATE TABLE IF NOT EXISTS `committees` (
+CREATE TABLE `committees` (
   `committeeID` int(11) NOT NULL,
   `committeeName` varchar(45) NOT NULL,
   `committeeDescription` varchar(150) NOT NULL
@@ -39,8 +39,7 @@ CREATE TABLE IF NOT EXISTS `committees` (
 -- Table structure for table `contact_us`
 --
 
-DROP TABLE IF EXISTS `contact_us`;
-CREATE TABLE IF NOT EXISTS `contact_us` (
+CREATE TABLE `contact_us` (
   `id` int(11) NOT NULL,
   `firstname` varchar(45) NOT NULL,
   `lastname` varchar(45) NOT NULL,
@@ -54,8 +53,7 @@ CREATE TABLE IF NOT EXISTS `contact_us` (
 -- Table structure for table `crew`
 --
 
-DROP TABLE IF EXISTS `crew`;
-CREATE TABLE IF NOT EXISTS `crew` (
+CREATE TABLE `crew` (
   `id` int(11) NOT NULL,
   `name` varchar(45) NOT NULL,
   `title` varchar(45) NOT NULL,
@@ -69,13 +67,29 @@ CREATE TABLE IF NOT EXISTS `crew` (
 -- Table structure for table `participants`
 --
 
-DROP TABLE IF EXISTS `participants`;
-CREATE TABLE IF NOT EXISTS `participants` (
+CREATE TABLE `participants` (
   `participantId` int(20) NOT NULL,
   `participantName` varchar(45) NOT NULL,
   `workshop_id` int(20) NOT NULL,
   `participant_email` varchar(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `workshops`
+--
+
+CREATE TABLE `workshops` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `instructor1_id` int(11) NOT NULL,
+  `instructor2_id` int(11) NOT NULL,
+  `hr_member1_id` int(11) NOT NULL,
+  `hr_member2_id` int(11) NOT NULL,
+  `overview` varchar(150) NOT NULL,
+  `committee_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
@@ -104,7 +118,17 @@ ALTER TABLE `crew`
 -- Indexes for table `participants`
 --
 ALTER TABLE `participants`
-  ADD PRIMARY KEY (`participantId`);
+  ADD PRIMARY KEY (`participantId`),
+  ADD UNIQUE KEY `workshop_fk_idx` (`workshop_id`);
+
+--
+-- Indexes for table `workshops`
+--
+ALTER TABLE `workshops`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `committee_id_fk_idx` (`committee_id`),
+  ADD UNIQUE KEY `instructor1_id_fk_idx` (`instructor1_id`),
+  ADD UNIQUE KEY `instructor2_id_fk_idx` (`instructor2_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -115,21 +139,25 @@ ALTER TABLE `participants`
 --
 ALTER TABLE `committees`
   MODIFY `committeeID` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `contact_us`
 --
 ALTER TABLE `contact_us`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `crew`
 --
 ALTER TABLE `crew`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `participants`
 --
 ALTER TABLE `participants`
   MODIFY `participantId` int(20) NOT NULL AUTO_INCREMENT;
+
 --
 -- Constraints for dumped tables
 --
@@ -139,6 +167,21 @@ ALTER TABLE `participants`
 --
 ALTER TABLE `crew`
   ADD CONSTRAINT `crew_ibfk_1` FOREIGN KEY (`committeeID`) REFERENCES `committees` (`committeeID`);
+
+--
+-- Constraints for table `participants`
+--
+ALTER TABLE `participants`
+  ADD CONSTRAINT `participants_ibfk_1` FOREIGN KEY (`workshop_id`) REFERENCES `workshops` (`id`);
+
+--
+-- Constraints for table `workshops`
+--
+ALTER TABLE `workshops`
+  ADD CONSTRAINT `workshops_ibfk_1` FOREIGN KEY (`committee_id`) REFERENCES `committees` (`committeeID`),
+  ADD CONSTRAINT `workshops_ibfk_2` FOREIGN KEY (`instructor1_id`) REFERENCES `crew` (`id`),
+  ADD CONSTRAINT `workshops_ibfk_3` FOREIGN KEY (`instructor2_id`) REFERENCES `crew` (`id`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
